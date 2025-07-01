2471a564a97d5652801c96c8b4b52673c59581fd
dhullubasnkot <123baglung@gmail.com>
2025-07-01 23:27:43 +0545

    Maintained

--- Weather-FrontEnd/src/index.jsx
+++ Weather-FrontEnd/src/index.jsx
@@ -1,7 +1,7 @@
-
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const r = createRoot(document.getElementById("root"));
 r.render(<App />);
+//index.jsx
